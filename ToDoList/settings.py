a1c7117578a2fcf4df64a60c6fc176ce344d14d1
NamikazeAsh <ashwinsatish0@gmail.com>
2022-11-23 16:50:48 +0530

    removing djangoonheroku

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -131,10 +131,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-import django_on_heroku
-django_on_heroku.settings(locals())
-
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
