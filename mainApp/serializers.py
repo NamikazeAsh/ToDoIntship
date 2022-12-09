ab46f059bd52df25f14b6bdef0fa2441d2294c7a
NamikazeAsh <84863353+NamikazeAsh@users.noreply.github.com>
2022-12-09 14:49:36 +0530

    primarykeyrelatedfield
    added pkrf

--- mainApp/serializers.py
+++ mainApp/serializers.py
@@ -9,15 +9,20 @@ class TagSerializer(serializers.ModelSerializer):
 
 class ToDoSerializer(serializers.ModelSerializer):
     
-    Tag = TagSerializer(many=True)
+    Tag = TagSerializer(many=True,read_only=True)
+    
+    TagID = serializers.PrimaryKeyRelatedField(
+        many=True,write_only = True,queryset = TagList.objects.all()
+    )
     
     class Meta:
         model = ToDo
-        fields = ('id','CreatedTimestamp','Title','Description','Date','Tag','Status')
-        
-        
-class ToDoSerializerC(serializers.ModelSerializer):
-    class Meta:
-        model = ToDo
-        fields = ('id','Title','Description','Date','Tag','Status')
+        fields = ('id','CreatedTimestamp','Title','Description','Date','Tag','TagID','Status')
+    
+    def create(self,validated_data):
+        tag = validated_data.pop("TagID",None)
+        todoz = ToDo.objects.create(**validated_data)
+        if tag:
+            todoz.Tag.set(tag)
+        return todoz
         
