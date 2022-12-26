b4a3fcb2ca66cb5f8d598ad27c5b9d0da32756b5
NamikazeAsh <ashwinsatish0@gmail.com>
2022-12-26 21:16:16 +0530

    gg1

--- mainApp/serializers.py
+++ mainApp/serializers.py
@@ -11,7 +11,6 @@ class ToDoSerializer(serializers.ModelSerializer):
     
     Tag = TagSerializer(many=True,read_only=True)
     
-<<<<<<< HEAD
     # TagID = serializers.PrimaryKeyRelatedField(
     #     many=True,write_only = True,queryset = TagList.objects.all()
     # )
@@ -27,20 +26,4 @@ class ToDoSerializer(serializers.ModelSerializer):
     #         todoz.Tag.set(tag)
     #     return todoz
         
-=======
-    TagID = serializers.PrimaryKeyRelatedField(
-        many=True,write_only = True,queryset = TagList.objects.all()
-    )
-    
-    class Meta:
-        model = ToDo
-        fields = ('id','CreatedTimestamp','Title','Description','Date','Tag','TagID','Status')
-    
-    def create(self,validated_data):
-        tag = validated_data.pop("TagID",None)
-        todoz = ToDo.objects.create(**validated_data)
-        if tag:
-            todoz.Tag.set(tag)
-        return todoz
->>>>>>> ab46f059bd52df25f14b6bdef0fa2441d2294c7a
         
